42122ecf44671d0fb777e9539d9765d917efdc5e
Lucas Silva <silva.lucasfeitoza@gmail.com>
2023-06-05 19:44:42 -0300

    Endpoints listando os parametros utilizados no calculo do custo postal

---
+++ src/PostagemFacil.Financeiro.API/Controllers/ParametrosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PostagemFacil.Financeiro.API.Data;
+
+namespace PostagemFacil.Financeiro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParametrosController : ControllerBase
+    {
+        private readonly FinanceiroContext _financeiroContext;
+
+        public ParametrosController(FinanceiroContext financeiroContext) => _financeiroContext = financeiroContext;
+
+        [HttpGet("pesos-limite")]
+        public async Task<IActionResult> ObterPesosLimite() => Ok(await _financeiroContext.PesosLimite.ToListAsync());
+
+        [HttpGet("tipos-caixa")]
+        public async Task<IActionResult> ObterTiposCaixa() => Ok(await _financeiroContext.TiposCaixa.ToListAsync());
+
+        [HttpGet("transportadoras")]
+        public async Task<IActionResult> ObterTransportadoras() => Ok(await _financeiroContext.Transportadoras.ToListAsync());
+    }
+}
